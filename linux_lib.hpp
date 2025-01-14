9245250bd7d78647c9e9d386ab86845512e3977a
Eero <eerolillemae@gmail.com>
2025-01-14 14:15:23 +0200

    added elapsed_since()

--- linux_lib.hpp
+++ linux_lib.hpp
@@ -4,11 +4,29 @@
 char *get_parent_path_noalloc(char *path);
 char *get_parent_path_alloc(const char *path);
 bool get_executable_path(char *buffer, int buffer_size);
+enum TimeUnit {
+	SEC = 1,
+	MS = 1000,
+	US = 1000000,
+	NS = 1000000000,
+};
+double elapsed_since(struct timespec then, enum TimeUnit units);
 
 #ifdef LINUX_LIB_INCLUDE_IMPLEMENTATION
 
 #include <unistd.h>
 // #include <fcntl.h>
+
+double elapsed_since(struct timespec then, enum TimeUnit units) {
+	struct timespec now = {};
+	clock_gettime(CLOCK_MONOTONIC, &now);
+
+	struct timespec elapsed;
+	elapsed.tv_sec = now.tv_sec - then.tv_sec;
+	elapsed.tv_nsec = now.tv_nsec - then.tv_nsec;
+
+	return elapsed.tv_sec * units + elapsed.tv_nsec * ((double)units / NS);
+}
 
 bool get_executable_path(char *buffer, int buffer_size)
 {
